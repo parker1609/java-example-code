af1b3ec3667fc800001d88eb7e1f266b6d9e98e3
CODEMCD <psbum1609@gmail.com>
2020-02-25 13:17:31 +0900

    add: clean coders fitness example

---
+++ java-simple-code/src/main/java/cleancoders/AfterRefactoringFitnessExample.java
@@ -0,0 +1,69 @@
+package cleancoders;
+
+import fitnesse.responders.run.SuiteResponder;
+import fitnesse.wiki.*;
+
+public class AfterRefactoringFitnessExample {
+    public String testableHtml(PageData pageData, boolean includeSuiteSetup) throws Exception {
+        return new SetUpTearDownSurrounder(pageData, includeSuiteSetup).surround();
+    }
+
+    private class SetUpTearDownSurrounder {
+        private PageData pageData;
+        private boolean includeSuiteSetup;
+        private WikiPage wikiPage;
+        private String content;
+
+        public SetUpTearDownSurrounder(PageData pageData, boolean includeSuiteSetup) {
+            this.pageData = pageData;
+            this.includeSuiteSetup = includeSuiteSetup;
+            wikiPage = pageData.getWikiPage();
+            content = new String();
+        }
+
+        public String surround() throws Exception {
+            if (ifTestPage())
+                surroundPageWithSetUpsAndTearDowns();
+            return pageData.getHtml();
+        }
+
+        private void surroundPageWithSetUpsAndTearDowns() throws Exception {
+            content = includeSetups();
+            content += pageData.getContent();
+            content += includeTearDowns();
+            pageData.setContent(content);
+        }
+
+        private boolean ifTestPage() throws Exception {
+            return pageData.hasAttribute("Test");
+        }
+
+        private String includeTearDowns() throws Exception {
+            String tearDowns = includeInherited("TearDown", "teardown");
+            if (includeSuiteSetup)
+                tearDowns += includeInherited(SuiteResponder.SUITE_TEARDOWN_NAME, "teardown");
+            return tearDowns;
+        }
+
+        private String includeSetups() throws Exception {
+            String setUps = "";
+            if (includeSuiteSetup)
+                setUps += includeInherited(SuiteResponder.SUITE_SETUP_NAME, "setup");
+            setUps += includeInherited("SetUp", "setup");
+            return setUps;
+        }
+
+        private String includeInherited(String pageName, String mode) throws Exception {
+            WikiPage suiteSetup = PageCrawlerImpl.getInheritedPage(pageName, wikiPage);
+            if (suiteSetup != null)
+                return includePage(suiteSetup, mode);
+            return "";
+        }
+
+        private String includePage(WikiPage suiteSetup, String mode) throws Exception {
+            WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(suiteSetup);
+            String pagePathName = PathParser.render(pagePath);
+            return String.format("!include -%s .%s\n", mode, pagePathName);
+        }
+    }
+}
---
+++ java-simple-code/src/main/java/cleancoders/FitnessExample.java
@@ -0,0 +1,67 @@
+package cleancoders;
+
+import fitnesse.responders.run.SuiteResponder;
+import fitnesse.wiki.*;
+
+public class FitnessExample {
+    public String testableHtml(PageData pageData, boolean includeSuiteSetup) throws Exception {
+        return new TestableHtmlBuilder(pageData, includeSuiteSetup).invoke();
+    }
+
+    private class TestableHtmlBuilder {
+        private PageData pageData;
+        private boolean includeSuiteSetup;
+        private WikiPage wikiPage;
+        private final StringBuffer buffer;
+
+        public TestableHtmlBuilder(PageData pageData, boolean includeSuiteSetup) {
+            this.pageData = pageData;
+            this.includeSuiteSetup = includeSuiteSetup;
+            this.wikiPage = pageData.getWikiPage();
+            this.buffer = new StringBuffer();
+        }
+
+        public String invoke() throws Exception {
+            if (isTestPage()) {
+                includeSetups();
+                buffer.append(pageData.getContent());
+                includeTearDowns();
+            }
+
+            pageData.setContent(buffer.toString());
+
+            return pageData.getHtml();
+        }
+
+        private boolean isTestPage() throws Exception {
+            return pageData.hasAttribute("Test");
+        }
+
+        private void includeTearDowns() throws Exception {
+            includeInherited("TearDown", "teardown");
+            if (includeSuiteSetup) {
+                includeInherited(SuiteResponder.SUITE_TEARDOWN_NAME, "teardown");
+            }
+        }
+
+        private void includeSetups() throws Exception {
+            if (includeSuiteSetup) {
+                includeInherited(SuiteResponder.SUITE_SETUP_NAME, "setup");
+            }
+            includeInherited("SetUp", "setup");
+        }
+
+        private void includeInherited(String pageName, String mode) throws Exception {
+            WikiPage suiteSetup = PageCrawlerImpl.getInheritedPage(pageName, wikiPage);
+            if (suiteSetup != null) {
+                includePage(suiteSetup, mode);
+            }
+        }
+
+        private void includePage(WikiPage suiteSetup, String mode) throws Exception {
+            WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(suiteSetup);
+            String pagePathName = PathParser.render(pagePath);
+            buffer.append("!include -" + mode + " .").append(pagePathName).append("\n");
+        }
+    }
+}
